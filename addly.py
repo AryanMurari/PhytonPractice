25cf89124a26477d0212b6c925ba30e9a513f377
Aryan Murari <92181578+AryanMurari@users.noreply.github.com>
2023-03-13 11:07:24 +0530

    Update addly.py

--- addly.py
+++ addly.py
@@ -1,3 +1,6 @@
+'''Take a string from user if the string is grater than 3 and ends with 'ing' add ly to it and print result
+else print original string'''
+
 a = input("Enter string: ")
 
 if len(a) > 3 and a.endswith("ing"):
